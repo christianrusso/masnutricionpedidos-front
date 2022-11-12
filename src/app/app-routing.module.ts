5154c548b2ab0e5b061500e0f7597ead4ebb62eb
Facundo <rusmanfacundo@gmail.com>
2022-11-12 18:40:16 -0300

    routing general sin resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,90 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DefaultLayoutComponent } from './containers';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'masnutricion',
-    pathMatch: 'full'
+    redirectTo: 'layout',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    component: DefaultLayoutComponent,
-    data: {
-      title: 'Home'
-    },
+    path: 'layout',
+    loadChildren: () =>
+      import('./views/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
     children: [
       {
-        path: 'masnutricion',
+        path: 'canal',
         loadChildren: () =>
-          import('./views/dashboard/dashboard.module').then((m) => m.DashboardModule)
+          import('./containers/canal/canal.module').then(
+            (m) => m.CanalModule
+          ),
       },
-
+      {
+        path: 'cliente',
+        loadChildren: () =>
+          import('./containers/cliente/cliente.module').then(
+            (m) => m.ClienteModule
+          ),
+      },
+      {
+        path: 'condiciones-venta',
+        loadChildren: () =>
+          import('./containers/condiciones-venta/condiciones-venta.module').then(
+            (m) => m.CondicionesVentaModule
+          ),
+      },
+      {
+        path: 'familia-producto',
+        loadChildren: () =>
+          import('./containers/familia-producto/familia-producto.module').then(
+            (m) => m.FamiliaProductoModule
+          ),
+      },
+      {
+        path: 'iva',
+        loadChildren: () =>
+          import('./containers/iva/iva.module').then(
+            (m) => m.IvaModule
+          ),
+      },
+      {
+        path: 'permiso',
+        loadChildren: () =>
+          import('./containers/permiso/permiso.module').then(
+            (m) => m.PermisoModule
+          ),
+      },
+      {
+        path: 'producto',
+        loadChildren: () =>
+          import('./containers/producto/producto.module').then(
+            (m) => m.ProductoModule
+          ),
+      },
+      {
+        path: 'regla-comercial',
+        loadChildren: () =>
+          import('./containers/regla-comercial/regla-comercial.module').then(
+            (m) => m.ReglaComercialModule
+          ),
+      },
+      {
+        path: 'telefono',
+        loadChildren: () =>
+          import('./containers/telefono/telefono.module').then(
+            (m) => m.TelefonoModule
+          ),
+      },
     ]
   },
 
 
+//falta login
 
-  {path: '**', redirectTo: 'masnutricion'}
+  { path: '**', redirectTo: 'layout' },
 ];
 
 @NgModule({
@@ -35,11 +92,10 @@ const routes: Routes = [
     RouterModule.forRoot(routes, {
       scrollPositionRestoration: 'top',
       anchorScrolling: 'enabled',
-      initialNavigation: 'enabledBlocking'
+      initialNavigation: 'enabledBlocking',
       // relativeLinkResolution: 'legacy'
-    })
+    }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
